db644477f0f83dcfab1d0657bf3feeb275e1930c
AlekseiiKanaiev <alexey.kanaev.ua@gmail.com>
2018-08-01 20:09:02 +0300

    added fibonachi function

---
+++ HomeWork/alekseii.kanaiev/hw1/src/fibonachi.ts
@@ -0,0 +1,20 @@
+//I dont know what exactly we need to recive, so i made tupple with both most frequent examples
+
+const fibonachi: Function = function(num:number):[Array<number>, Array<number>]{
+    const tupple: [Array<number>, Array<number>] = [[0], [0]];
+    if (num <= 0){
+        return tupple;
+    }
+    for (let i = 0, k = 1, m = 1; i < num-1 || m+tupple[1][i] <= num; i++){
+        if(i < num-1){
+            tupple[0].push(k+tupple[0][i]);
+            k = tupple[0][i];
+        } 
+        if(m+tupple[1][i] <= num){
+            tupple[1].push(m+tupple[1][i]);
+            m = tupple[1][i];
+        } 
+        
+    }
+    return tupple;
+}

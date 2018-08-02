24f2da9bfbf6e4bd5e0fbf736cda0f37e5ace65c
Andrey Skumin <andrey.skumin@p2h.com>
2018-08-02 12:23:10 +0300

    up

--- HomeWork/andrey.skumin/hw1/main.ts
+++ HomeWork/andrey.skumin/hw1/main.ts
@@ -1,10 +1,10 @@
 const toStr = (state: boolean): string => state ? '1' : '0';
-const factorial = (n: number): number => n ? n * factorial(n - 1) : 1;
+const factorial = (n: number): number => n > 0 ? n * factorial(n - 1) : 1;
 const fibonacci = (n: number): number => {
   let a = 1;
   let b = 1;
   for (let i = 3; i <= n; i++) {
-    let c = a + b;
+    let c: number = a + b;
     a = b;
     b = c;
   }

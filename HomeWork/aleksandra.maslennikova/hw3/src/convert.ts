65832e9b1a39d105cdd9bf9e74c32a6c22d5f680
aleksandra-maslennikova <alex.maslennikova19@gmail.com>
2018-08-17 15:25:00 +0300

    finished

--- HomeWork/aleksandra.maslennikova/hw3/src/convert.ts
+++ HomeWork/aleksandra.maslennikova/hw3/src/convert.ts
@@ -1,17 +1,7 @@
-/*# Homework 3
-- Create interface Convert. It contains two methods from, to.
-    - From retrieves decimal number returns string.
-    - To retrieves string returns decimal number.
-- Interface should be implemented by three classes
-    - BinaryConverter -> convert decimal to binary
-    - OctalConverter -> convert decimal to octal
-    - HexadecimalConverter -> convert hex to decimal.
-    - Forbidden use external convert functions only * and /; */
-
-
 interface Convert {
   from(number: number): string;
   to(number: string): number;
+  division(number: number): number;
 }
 
 class BinaryConverter implements Convert {
@@ -29,46 +19,122 @@ class BinaryConverter implements Convert {
     return result.reverse().join('')
 
   }
+
   division(number: number): number {
     const a = number / 2;
     return a
   }
+
   to(number: string): number {
     const arr = number.split('').reverse();
-    const result = arr.reduce((acc, item, index)=>{
+    const result = arr.reduce((acc, item, index) => {
       return (item === '1') ? acc + Math.pow(2, index) : acc;
     }, 0);
-
-    console.log(result);
     return result;
-
-
   }
+
 }
 
 const converter = new BinaryConverter;
+
 console.log(converter.from(25));
 console.log(converter.from(0));
 console.log(converter.to('111'));
 console.log(converter.to('1111'));
-/*class ClassicalMusic implements Music {
-   play(trackName:string): void {
-       console.log(`Mozart playing ${trackName}`);
-   }
 
-   public violin: boolean = true;
+
+class OctalConverter implements Convert {
+
+  from(number: number): string {
+    let quotient: number = number;
+    let result: number[] = [];
+
+    while (quotient >= 8) {
+      const sum: number = ~~this.division(quotient);
+      const rest: number = quotient - (sum * 8);
+      result.push(rest);
+      quotient = sum;
+    }
+    result.push(quotient);
+    return result.reverse().join('')
+
+  }
+  division(number: number): number {
+    const a = number / 8;
+    return a
+  }
+  to(number: string): number {
+    const arr = number.split('').reverse();
+    const result = arr.reduce((acc, item, index) => {
+      return acc + (Math.pow(8, index) * Number(item));
+    }, 0);
+
+    return result;
+  }
 }
 
-class RockMusic implements Music, Drum {
-  tuc(): void {
-      console.log('Tuc tuc tuc');
-  }
-  play(trackName:string): void {
-      console.log(`THUNDER!!!! THUNDER!!! ${trackName}!!!!`);
-  }
+const oconverter = new OctalConverter;
+console.log(oconverter.from(58));
+console.log(oconverter.from(125));
+console.log(oconverter.to('15'));
+console.log(oconverter.to('75'));
 
-  public guitar: boolean = true;
+class HexadecimalConverter implements Convert {
+
+  from(number: number): string {
+    let quotient: number = number;
+    let result: number[] = [];
+    const letters: object = {
+      10: 'A',
+      11: 'B',
+      12: 'C',
+      13: 'D',
+      14: 'E',
+      15: 'F'
+    }
+
+    while (quotient >= 16) {
+      const sum: number = ~~this.division(quotient);
+      let rest: number = quotient - (sum * 16);
+      if (rest > 9) {
+        rest = letters[rest]
+      }
+      result.push(rest);
+      quotient = sum;
+
+      if (quotient > 9) {
+        quotient = letters[quotient];
+      }
+      result.push(quotient);
+      return result.reverse().join('')
+
+    }
+  }
+  division(number: number): number {
+    const a = number / 16;
+    return a
+  }
+  to(number: string): number {
+    const numbers: object = {
+      'A': 10,
+      'B': 11,
+      'C': 12,
+      'D': 13,
+      'E': 14,
+      'F': 15
+    }
+    const arr = number.split('').reverse();
+    const result = arr.reduce((acc, item, index) => {
+      const convertedNumber = numbers[item] ? numbers[item] : Number(item);
+      return acc + (Math.pow(16, index) * convertedNumber);
+    }, 0);
+
+    return result;
+  }
 }
 
-const music: Music = new ClassicalMusic();
-let rockNRoll: Drum = new RockMusic();*/
+const hconverter = new HexadecimalConverter;
+console.log(hconverter.from(58));
+console.log(hconverter.from(125));
+console.log(hconverter.to('3A'));
+console.log(hconverter.to('2F'));

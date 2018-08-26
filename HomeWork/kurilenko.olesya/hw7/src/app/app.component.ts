512b1374a16760131d8409a42e7e07f3c6165b02
Olesya Kurilenko <olesya.kurilenko@gmail.com>
2018-08-26 10:54:47 +0300

    hw7 - calculator

---
+++ HomeWork/kurilenko.olesya/hw7/src/app/app.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+
+export enum EOperator {
+  clear = 0,
+  multiplication = 1, // *
+  division = 2, // /
+  subtraction = 3, // -
+  addition = 4, // +
+  equally = 5, // =
+  number = 6, // number
+  point = 7 // .
+}
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  EOperator = EOperator;
+  public total: string = '';
+
+  public operatorClick(operator: EOperator, value?: number | string) {
+    console.log(this.total + '  operator' + operator + ' value' + value);
+    switch (operator) {
+      case EOperator.clear:
+        this.total = '';
+        break;
+      case EOperator.multiplication:
+        this.total = this.replaseLastOperator(this.total, '*');
+        break;
+      case EOperator.division:
+        this.total = this.replaseLastOperator(this.total, '/');
+        break;
+      case EOperator.subtraction:
+        this.total = this.replaseLastOperator(this.total, '-');
+        break;
+      case EOperator.addition:
+        this.total = this.replaseLastOperator(this.total, '+');
+        break;
+      case EOperator.equally:
+        this.total = this.calculation(this.total);
+        this.total = this.total.toString();
+        break;
+      case EOperator.number:
+        this.total += value;
+        break;
+      case EOperator.point:
+        (this.total[this.total.length - 1] === '.' && value === '.') ?
+          '' :
+          this.total += value;
+        break;
+      default:
+        break;
+    }
+
+  }
+  private replaseLastOperator(value: string, replOper: string) {
+    const lastOper: string = value[value.length - 1];
+    if (lastOper === '*' || lastOper === '+' || lastOper === '/' || lastOper === '-') {
+      return (value.substring(0, value.length - 1) + replOper);
+    }
+    return value + replOper;
+  }
+
+  private calculation(value: string) {
+    return eval(this.replaseLastOperator(value,''));
+  }
+}
+
+
+
+

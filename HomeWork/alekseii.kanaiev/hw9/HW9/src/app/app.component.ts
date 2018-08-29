a812e179d2e2e9f1494ab3d7f1ae28b61943f3fa
AlekseiiKanaiev <alexey.kanaev.ua@gmail.com>
2018-08-29 13:08:53 +0300

    alekseii.kanaev added hw9 - pipes

---
+++ HomeWork/alekseii.kanaiev/hw9/HW9/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'HW9';
+  public str: string;
+  public morseStr: string;
+}

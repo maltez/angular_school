2390f3a9a8e69458a4e73b552c81f6d565ccc492
Sergey Malahov <annushka171@gmail.com>
2018-09-18 00:57:48 +0300

    hw11 reducer

---
+++ HomeWork/sergey.malahov/hw11/calculator3/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {ButtonComponent} from "./button/button.component";
+
+@NgModule({
+  declarations: [
+    AppComponent, ButtonComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
